ad61cfde698891e059883ac180bb2fdf57cfd0d2
Nick Culbertson <nickculbertson@gmail.com>
2015-04-10 09:42:29 -0500

    First Commit
    Created New Xcode Project

---
+++ Complete iOS Toolkit/Complete iOS Toolkit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Complete iOS Toolkit
+//
+//  Created by Nick Culbertson on 4/10/15.
+//  Copyright (c) 2015 Nick Culbertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Complete iOS Toolkit/Complete iOS Toolkit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Complete iOS Toolkit
+//
+//  Created by Nick Culbertson on 4/10/15.
+//  Copyright (c) 2015 Nick Culbertson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
