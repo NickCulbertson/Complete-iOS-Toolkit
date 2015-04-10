20e6d4a0057834a34fb743fed757b148172aac7f
Nick Culbertson <nickculbertson@gmail.com>
2015-04-10 13:11:15 -0500

    Updated UI
    Added new UI components.

--- Complete iOS Toolkit/Complete iOS Toolkit/ViewController.m
+++ Complete iOS Toolkit/Complete iOS Toolkit/ViewController.m
@@ -19,6 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
