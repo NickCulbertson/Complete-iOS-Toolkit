de46988d293c55d0450099a8f60e996e6c00e113
Nick Culbertson <nickculbertson@gmail.com>
2015-04-11 17:32:35 -0500

    JSON to UI
    Added Color Change with int

--- Complete iOS Toolkit/Complete iOS Toolkit/ViewController.h
+++ Complete iOS Toolkit/Complete iOS Toolkit/ViewController.h
@@ -8,20 +8,31 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController{
+@interface ViewController : UIViewController<UITableViewDelegate, UIWebViewDelegate>{
     
     IBOutlet UIImageView *StatusBar;
     IBOutlet UIImageView *HeaderShadow;
     IBOutlet UIScrollView *HeaderContainer;
     IBOutlet UILabel *HeaderLabel;
+    IBOutlet UITableView *MenuTable;
+    IBOutlet UIScrollView *WebViewContainer;
     
     IBOutlet UIButton *MenuButton;
     IBOutlet UIButton *DetailsButton;
     
+    IBOutlet UIWebView *WebView;
+    IBOutlet UIToolbar *WebViewControls;
+    IBOutlet UIActivityIndicatorView *ActivityIndicator;
+    
     IBOutlet UIImageView *LogoImageView;
     IBOutlet UIImageView *BackgroundImageView;
-    
+    IBOutlet UIImageView *DarkImageView;
+    BOOL MenuShow;
     NSArray *SettingsArray;
+    NSArray *MenuArray;
+    NSMutableArray *ContentArray;
+    
+    int MenuItems;
 }
 - (IBAction)MenuAction;
 - (IBAction)DetailsAction;
--- Complete iOS Toolkit/Complete iOS Toolkit/ViewController.m
+++ Complete iOS Toolkit/Complete iOS Toolkit/ViewController.m
@@ -15,6 +15,13 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    DarkImageView.alpha=0;
+    MenuItems=0;
+    ContentArray = [[NSMutableArray alloc] initWithObjects:nil];
+    WebView.delegate = self;
+    [WebView addSubview:ActivityIndicator];
+    WebView.scalesPageToFit = YES;
+    
     
 //    //Blue
 //    StatusBar.backgroundColor = [UIColor colorWithRed:0.0f/255.0f green:180.0f/255.0f blue:240.0f/255.0f alpha:1];
@@ -45,7 +52,6 @@
     
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"local" ofType:@"json"];
     NSData* data = [NSData dataWithContentsOfFile:filePath];
-    //NSData* data = [filePath dataUsingEncoding:NSUTF8StringEncoding];
     
     
     NSLog(@"%@",filePath);
@@ -59,8 +65,6 @@
 - (void)fetchedDataLocal:(NSData *)responseData {
     //parse out the json data
     NSError* error;
-    
-    
     
     if(responseData==nil){
         
@@ -76,57 +80,229 @@
         
         
         SettingsArray = [json objectForKey:@"AppSettings"]; //2
+        MenuArray = [json objectForKey:@"MenuItems"];
+        
+        MenuItems = 4;
+        
+        NSString* HeaderColorString = [SettingsArray[0] objectForKey:@"HeaderColor"];
+        NSString* HeaderLabelString = [SettingsArray[1] objectForKey:@"HeaderLabel"];
+        
+        NSString* MenuItemsString;
+        for (int i=0; i <= MenuItems; i++) {
+            MenuItemsString = [MenuArray[i] objectForKey:@"MenuTitle"];
+            [ContentArray addObject:MenuItemsString];
+        }
+
+        [MenuTable reloadData];
+        
         if(SettingsArray==NULL){
             
-            //Red
-            StatusBar.backgroundColor = [UIColor colorWithRed:233.0f/255.0f green:72.0f/255.0f blue:72.0f/255.0f alpha:1.0];
-            HeaderContainer.backgroundColor = [UIColor colorWithRed:233.0f/255.0f green:72.0f/255.0f blue:72.0f/255.0f alpha:1.0];
-            HeaderShadow.backgroundColor = [UIColor colorWithRed:190.0f/255.0f green:22.0f/255.0f blue:22.0f/255.0f alpha:1.0];
+            
             
             NSLog(@"JSON Object Not There");
             
             
            // [self Here];
         }else{
+            MenuTable.center=CGPointMake(-[MenuTable bounds].size.width/2,MenuTable.center.y);
             
+            HeaderLabel.text=HeaderLabelString;
             
+            float r;
+            float g;
+            float b;
+            float rShadow;
+            float gShadow;
+            float bShadow;
             
-//            contentArray = [[NSArray alloc] initWithObjects:@"BOLLYWOOD RADIO",stations[1],stations[3],stations[5],stations[7],stations[9],stations[11],stations[13],stations[15],stations[17],stations[19],stations[21],stations[23],stations[25],@"",@"BEYOND BOLLY",stations[27],stations[29],stations[31],stations[33],stations[35],stations[37],stations[39],stations[41],@"",@"STATION LINKS",@"",@"MORE RADIO APPS!!!",@"more stations/no ads",@"",@"",@"",@"",@"",@"",@"",nil];
-//            
-//            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-//            [defaults setValue:stations forKey:@"fieldKey1"];
-//            [defaults synchronize];
-//            
-//            stations = [NSArray arrayWithArray:[[NSUserDefaults standardUserDefaults] objectForKey:@"fieldKey1"]];
-//            
-//            contentArray = [[NSArray alloc] initWithObjects:@"BOLLYWOOD RADIO",stations[1],stations[3],stations[5],stations[7],stations[9],stations[11],stations[13],stations[15],stations[17],stations[19],stations[21],stations[23],stations[25],@"",@"BEYOND BOLLY",stations[27],stations[29],stations[31],stations[33],stations[35],stations[37],stations[39],stations[41],@"",@"STATION LINKS",@"",@"MORE RADIO APPS!!!",@"more stations/no ads",@"",@"",@"",@"",@"",@"",@"",nil];
-//            
-//            if([[stations objectAtIndex:0] isEqual:textField.text]){
-//                
-//                NSLog(@"New Station");
-//            }else{
-//                if(Countday==1){
-//                    // [self next];
-//                }
-//                NSLog(@"Up To Date");
-//            }
-//            [self.table1 reloadData];
-//            
-//            NSDateFormatter *format = [[NSDateFormatter alloc] init];
-//            format.dateFormat = @"dd-MM-yyyy";
-//            
-//            NSDate *now = [[NSDate alloc] init];
-//            
-//            NSString *theDate = [format stringFromDate:now];
-//            
-//            NSLog(@"%@",theDate);
-//            
-//            
-//            NSUserDefaults *defaults2 = [NSUserDefaults standardUserDefaults];
-//            [defaults2 setValue:theDate forKey:@"fieldKey2"];
-//            [defaults2 synchronize];
+            if([HeaderColorString isEqualToString:@"1"]){
+                //Red
+                r=26;
+                g=188;
+                b=156;
+                rShadow=22;
+                gShadow=160;
+                bShadow=133;
+            }else if([HeaderColorString isEqualToString:@"2"]){
+                //Light Blue
+                r=46;
+                g=204;
+                b=113;
+                rShadow=39;
+                gShadow=174;
+                bShadow=96;
+            }else if([HeaderColorString isEqualToString:@"3"]){
+                //Light Blue
+                r=52;
+                g=152;
+                b=219;
+                rShadow=41;
+                gShadow=128;
+                bShadow=185;
+            }else if([HeaderColorString isEqualToString:@"4"]){
+                //Light Blue
+                r=155;
+                g=89;
+                b=182;
+                rShadow=142;
+                gShadow=68;
+                bShadow=173;
+            }else if([HeaderColorString isEqualToString:@"5"]){
+                //Light Blue
+                r=52;
+                g=73;
+                b=94;
+                rShadow=44;
+                gShadow=62;
+                bShadow=80;
+            }else if([HeaderColorString isEqualToString:@"6"]){
+                //Light Blue
+                r=241;
+                g=196;
+                b=15;
+                rShadow=243;
+                gShadow=156;
+                bShadow=18;
+            }else if([HeaderColorString isEqualToString:@"7"]){
+                //Light Blue
+                r=231;
+                g=76;
+                b=60;
+                rShadow=192;
+                gShadow=57;
+                bShadow=43;
+            }else if([HeaderColorString isEqualToString:@"8"]){
+                //Light Blue
+                r=236;
+                g=240;
+                b=241;
+                rShadow=189;
+                gShadow=195;
+                bShadow=199;
+            }else if([HeaderColorString isEqualToString:@"9"]){
+                //Light Blue
+                r=149;
+                g=165;
+                b=166;
+                rShadow=127;
+                gShadow=140;
+                bShadow=141;
+            }
+            StatusBar.backgroundColor = [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:1.0];
+            HeaderContainer.backgroundColor = [UIColor colorWithRed:r/255.0f green:g/255.0f blue:b/255.0f alpha:1.0];
+            HeaderShadow.backgroundColor = [UIColor colorWithRed:rShadow/255.0f green:gShadow/255.0f blue:bShadow/255.0f alpha:1.0];
+            WebViewControls.translucent=NO;
+            WebViewControls.barTintColor=[UIColor colorWithRed:rShadow/255.0f green:gShadow/255.0f blue:bShadow/255.0f alpha:1.0];
         }
     }
+}
+
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+// Customize the number of rows in the table view.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    //return [contentArray count];
+    return MenuItems;
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Set up the cell...
+    
+    cell.textLabel.text = [ContentArray objectAtIndex:indexPath.row];
+    return cell;
+    
+    
+    
+}
+
+
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    if(section == 0){
+        
+        return @"MENU";
+    }
+    return 0;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSString* MenuItemString;
+    NSString* HeaderLabelString;
+    
+    for (int i=0; i <= MenuItems; i++) {
+        if(indexPath.row == i){
+            MenuItemString = [MenuArray[i] objectForKey:@"MenuURL"];
+            HeaderLabelString = [MenuArray[i] objectForKey:@"MenuLabel"];
+        }
+    }
+//    if(indexPath.row == 0){
+//        
+//
+//    }else if(indexPath.row == 1){
+//        MenuItemString = [SettingsArray[12] objectForKey:@"MenuURL2"];
+//        HeaderLabelString = [SettingsArray[9] objectForKey:@"MenuLabel2"];
+//
+//    }else if(indexPath.row == 2){
+//        MenuItemString = [SettingsArray[13] objectForKey:@"MenuURL3"];
+//        HeaderLabelString = [SettingsArray[10] objectForKey:@"MenuLabel3"];
+//        
+//    }else if(indexPath.row == 3){
+//        MenuItemString = [SettingsArray[14] objectForKey:@"MenuURL3"];
+//        HeaderLabelString = [SettingsArray[10] objectForKey:@"MenuLabel3"];
+//        
+//    }else if(indexPath.row == 4){
+//        MenuItemString = [SettingsArray[15] objectForKey:@"MenuURL3"];
+//        HeaderLabelString = [SettingsArray[10] objectForKey:@"MenuLabel3"];
+//       
+//    }else if(indexPath.row == 5){
+//        
+//    }else if(indexPath.row == 6){
+//       
+//    }else if(indexPath.row == 7){
+//        
+//    }else if(indexPath.row == 8){
+//       
+//    }else if(indexPath.row == 9){
+//        
+//    }else if(indexPath.row == 10){
+//        
+//    }
+    WebView.alpha=0;
+    [WebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"about:blank"]]];
+
+    [WebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:MenuItemString]]];
+    HeaderLabel.text=HeaderLabelString;
+
+    WebViewContainer.center=CGPointMake([[UIScreen mainScreen] bounds].size.width,WebViewContainer.center.y);
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:.5];
+    [UIView setAnimationDelay:0];
+    [UIView setAnimationRepeatCount:0];
+    WebView.alpha=1;
+    MenuTable.center=CGPointMake(-[MenuTable bounds].size.width/2,MenuTable.center.y);
+    WebViewContainer.center=CGPointMake([[UIScreen mainScreen] bounds].size.width/2,WebViewContainer.center.y);
+    DarkImageView.alpha=0;
+    [UIView commitAnimations];
+    MenuShow=false;
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
 }
 
 -(UIStatusBarStyle)preferredStatusBarStyle{
@@ -139,8 +315,31 @@
 }
 
 - (IBAction)MenuAction {
+    if (!MenuShow) {
+        [UIView beginAnimations:nil context:NULL];
+        [UIView setAnimationDuration:.5];
+        [UIView setAnimationDelay:0];
+        [UIView setAnimationRepeatCount:0];
+    
+        MenuTable.center=CGPointMake([MenuTable bounds].size.width/2,MenuTable.center.y);
+        DarkImageView.alpha=.5;
+        [UIView commitAnimations];
+        MenuShow=true;
+    }else{
+        [UIView beginAnimations:nil context:NULL];
+        [UIView setAnimationDuration:.5];
+        [UIView setAnimationDelay:0];
+        [UIView setAnimationRepeatCount:0];
+        
+        MenuTable.center=CGPointMake(-[MenuTable bounds].size.width/2,MenuTable.center.y);
+        DarkImageView.alpha=0;
+        [UIView commitAnimations];
+        MenuShow=false;
+    }
+    
 }
 
 - (IBAction)DetailsAction {
+    
 }
 @end
